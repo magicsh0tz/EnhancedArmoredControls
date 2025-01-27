35d463e0fb6755667810407d71edf234cfa17321
magicsh0tz <magicsh0tz@users.noreply.github.com>
2025-01-27 19:59:17 +0800

    Convert indentation to spaces

--- addons/mgc_eac/config.cpp
+++ addons/mgc_eac/config.cpp
@@ -1,35 +1,35 @@
 #include "script_component.hpp"
 
 #define MACRO_ADD_ACTIONS \
-	class UserActions { \
-		class VAR(takeControls) { \
-			displayName = "Take controls"; \
-			displayNameDefault = ""; \
-			priority = 10; \
-			radius = 10; \
-			position = ""; \
-			showWindow = 0; \
-			hideOnUse = 1; \
-			onlyForPlayer = 0; \
-			shortcut = ""; \
-			condition = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(canTakeControls)); \
-			statement = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(takeControls);); \
-		}; \
-		\
-		class VAR(releaseControls) { \
-			displayName = "Release controls"; \
-			displayNameDefault = ""; \
-			priority = 10; \
-			radius = 10; \
-			position = ""; \
-			showWindow = 0; \
-			hideOnUse = 1; \
-			onlyForPlayer = 0; \
-			shortcut = ""; \
-			condition = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(canReleaseControls)); \
-			statement = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(releaseControls);); \
-		}; \
-	};
+    class UserActions { \
+        class VAR(takeControls) { \
+            displayName = "Take controls"; \
+            displayNameDefault = ""; \
+            priority = 10; \
+            radius = 10; \
+            position = ""; \
+            showWindow = 0; \
+            hideOnUse = 1; \
+            onlyForPlayer = 0; \
+            shortcut = ""; \
+            condition = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(canTakeControls)); \
+            statement = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(takeControls);); \
+        }; \
+        \
+        class VAR(releaseControls) { \
+            displayName = "Release controls"; \
+            displayNameDefault = ""; \
+            priority = 10; \
+            radius = 10; \
+            position = ""; \
+            showWindow = 0; \
+            hideOnUse = 1; \
+            onlyForPlayer = 0; \
+            shortcut = ""; \
+            condition = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(canReleaseControls)); \
+            statement = QUOTE([ARR_2(([] call FUNC(getPlayer)),this)] call FUNC(releaseControls);); \
+        }; \
+    };
 
 class CfgPatches {
     class NAME {
@@ -50,8 +50,8 @@ class CfgVehicles {
         MACRO_ADD_ACTIONS
     };
 
-	class Tank;
-	class Tank_F: Tank {
-		MACRO_ADD_ACTIONS
-	};
+    class Tank;
+    class Tank_F: Tank {
+        MACRO_ADD_ACTIONS
+    };
 };
